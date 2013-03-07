490441c3a3ed1578f535bf80d8e69baaab73f1b1
c <c@X1Carbon.(none)>
2013-03-07 15:53:54 -0600

    added some ec2 stuff

--- config.R
+++ config.R
@@ -5,11 +5,17 @@
 queue <- "Q1"
 # aws account number stored in separate file
 aws.account <- as.character(scan("~/.awsaccount", quiet=T))
-ami <- "ami id here"
-aws.zone <- "aws availability zone here"
-
-max.nodes <- "maximum number of nodes to use"
-allowable.time <- "max allowable time between tasks for a machine"
+# bioconductor ami http://bioconductor.org/help/bioconductor-cloud-ami/#ami_ids
+ami.id <- "ami-910c83f8"
+aws.availability.zone <- "us-east-1a"
+ec2.instance.type <- "t1.micro"  # "m1.small"
+# startup script for an EC2 instance
+user.data.file <- ""
+# maximum number of worker nodes to use
+max.nodes <- 3
+# allowable time in seconds between tasks for a machine before intervention
+# by the controller machine
+allowable.time <- 180
 
 # these have private info, will strip out and move to main area soon
 path.to.ec2.shell.scripts <- "AWS_scripts"
