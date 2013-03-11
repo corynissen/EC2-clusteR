6cd3beb31c582ff19d10c5581534ab626161c02f
c <c@X1Carbon.(none)>
2013-03-11 12:28:48 -0500

    changed machine to instance

--- AWS_scripts/write_to_dynamo.py
+++ AWS_scripts/write_to_dynamo.py
@@ -2,7 +2,7 @@ import boto, sys
 
 tablename = sys.argv[1]
 messageid = sys.argv[2]
-machineid = sys.argv[3]
+instanceid = sys.argv[3]
 timestamp_pretty = sys.argv[4]
 timestamp_int = sys.argv[5]
 
@@ -11,11 +11,11 @@ conn = boto.connect_dynamodb()
 table = conn.get_table(tablename)
 item_data = {
         'messageid': messageid,
-        'machineid': machineid,
+        'instanceid': instanceid,
         'timestamp': timestamp_pretty,
     }
-item = table.new_item(hash_key=machineid,range_key=int(timestamp_int), 
+item = table.new_item(hash_key=instanceid,range_key=int(timestamp_int), 
                       attrs=item_data)
 item.put()
 
-# python write_to_dynamo.py 'cory_test' 'messageid1' 'machineid1', '12/9/2011 11:36:03 PM' 1362599095
+# python write_to_dynamo.py 'cory_test' 'messageid1' 'instanceid1', '12/9/2011 11:36:03 PM' 1362599095
