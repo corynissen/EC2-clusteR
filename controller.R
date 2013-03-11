75acd7b8376563c92205cf8c45de0a57217eb329
c <c@X1Carbon.(none)>
2013-03-11 12:29:59 -0500

    added real code for the first time

--- controller.R
+++ controller.R
@@ -1,5 +1,7 @@
 
 # this script will manage the starting / stopping of worker nodes.
+install.packages("RJSONIO", repos="http://cran.mtu.edu/")
+library(RJSONIO)
 
 # if local file exists, source it, otherwise source the default one.
 config_file <- ifelse((file.exists("config/config_local.R")),
@@ -10,17 +12,17 @@ source(config_file)
 # it must be able to read from the queue to get a task, do the task,
 # and write back to the queue indicating the task is done
 
-run <- function(queue=queue, func=some.function, aws.access.key=aws.access.key,
-                aws.secret.key=aws.secret.key, ami=ami, aws.zone=aws.zone,
-                max.nodes=max.nodes){
+run <- function(queue, max.nodes, ami.id, ec2.instance.type, aws.availability.zone, path.to.ec2.shell.scripts, allowable.time, aws.account){
   # start machines
   # change to apply-ish function after working
   ec2.machine.id.vec <- NULL
   for(i in 1:max.nodes){
     print(paste("starting machine number", i, "of", max.nodes))
     ec2.machine.id.vec <- c(ec2.machine.id.vec,
-                            start.ec2.machine(aws.access.key, aws.secret.key,
-                                              ami, aws.zone))
+                            start.ec2.machine(ami.id=ami.id,
+                                 ec2.instance.type=ec2.instance.type,
+                                 aws.availability.zone=aws.availability.zone,
+                                 path.to.ec2.shell.scripts=path.to.ec2.shell.scripts))
   }
 
   ### monitoring
@@ -30,17 +32,27 @@ run <- function(queue=queue, func=some.function, aws.access.key=aws.access.key,
   # left in the queue, start another machine
   while(length(ec2.machine.id.vec) > 0){
     for(machine in ec2.machine.id.vec){
-      last.task.time <- read.task.log.from.dynamo(machine)
-      if(Sys.time() - last.task.time > allowable.time){
-        stop.ec2.machine(aws.access.key=aws.access.key, aws.secret.key,
-                         machine.id=machine.id)
-        # remove stopped machine from list
-        ec2.machine.id.vec <- ec2.machine.id.vec[ec2.machine.id.vec != machine]
-        # start new machine if there are still items left in queue
-        if(get.queue.length(queue) > 0){
-          ec2.machine.id.vec <- c(ec2.machine.id.vec,
-                                  start.ec2.machine(aws.access.key, aws.secret.key,
-                                                    ami, aws.zone))
+      read.message.log.json <- read.message.log.from.dynamo(path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
+                                    table.name=log.table.name, instance.id=machine)
+      # amazon may return single quotes instead of required double quotes
+      log.list <- fromJSON(gsub("'", '"', read.message.log.json$Count))
+      log.count <- log.list$Count
+      if(log.count>0){
+        last.message.time <- log.list$Items[[1]]$datetime
+        if(unclass(Sys.time()) - last.message.time > allowable.time){
+          stop.ec2.machine(instance.id=machine, path.to.ec2.shell.scripts=path.to.ec2.shell.scripts)
+          # remove stopped machine from list
+          ec2.machine.id.vec <- ec2.machine.id.vec[ec2.machine.id.vec != machine]
+          # start new machine if there are still items left in queue
+          queue.length <- get.queue.length(path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
+                                         aws.account=aws.account, queue=queue)
+          if(as.numeric(queue.length) > 10){
+            ec2.machine.id.vec <- c(ec2.machine.id.vec,
+                                  start.ec2.machine(ami.id=ami.id,
+                                  ec2.instance.type=ec2.instance.type,
+                                  aws.availability.zone=aws.availability.zone,
+                                  path.to.ec2.shell.scripts=path.to.ec2.shell.scripts))
+          }
         }
       }
     }
@@ -52,3 +64,9 @@ run <- function(queue=queue, func=some.function, aws.access.key=aws.access.key,
 #                     machine.id=machine.id)
 #  }
 }
+
+#run(queue=queue, max.nodes=max.nodes, ami.id=ami.id,
+#    ec2.instance.type=ec2.instance.type,
+#    aws.availability.zone=aws.availability.zone,
+#    path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
+#    allowable.time=allowable.time, aws.account=aws.account)
