88ed591c6cc0e5a2fe474e3e67e993e495735f5d
c <c@X1Carbon.(none)>
2013-03-12 14:32:36 -0500

    added a bunch of stuff to make it actually work.

--- controller.R
+++ controller.R
@@ -1,7 +1,9 @@
 
 # this script will manage the starting / stopping of worker nodes.
-install.packages("RJSONIO", repos="http://cran.mtu.edu/")
-library(RJSONIO)
+a <- try(library(RJSONIO), silent=T)
+if(class(a)=="try-error"){
+  install.packages("RJSONIO", repos="http://cran.mtu.edu/")
+}
 
 # if local file exists, source it, otherwise source the default one.
 config_file <- ifelse((file.exists("config/config_local.R")),
@@ -13,49 +15,81 @@ source("helper.R")
 # it must be able to read from the queue to get a task, do the task,
 # and write back to the queue indicating the task is done
 
-run <- function(queue, max.nodes, ami.id, ec2.instance.type, aws.availability.zone, path.to.ec2.shell.scripts, allowable.time, aws.account, user.data.file, ec2.key, ec2.security.group){
+run <- function(queue, max.nodes, ami.id, ec2.instance.type, aws.availability.zone, path.to.ec2.shell.scripts, allowable.time, aws.account, user.data.file, ec2.key, ec2.security.group, log.table.name){
   # start machines
   # change to apply-ish function after working
-  ec2.machine.id.vec <- NULL
+  start.time <- unclass(Sys.time())
+  instance.log <- data.frame(instance.id=c(), start.time=c(), stringsAsFactors=F)
   for(i in 1:max.nodes){
     print(paste("starting machine number", i, "of", max.nodes))
-    ec2.machine.id.vec <- c(ec2.machine.id.vec,
-                            start.ec2.machine(ami.id=ami.id,
+    new.instance.id <- start.ec2.machine(ami.id=ami.id,
                                  ec2.instance.type=ec2.instance.type,
                                  aws.availability.zone=aws.availability.zone,
                                  path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
                                  user.data.file=user.data.file, key=ec2.key,
-                                 group=ec2.security.group))
+                                 group=ec2.security.group)
+    new.row <- data.frame(instance.id=new.instance.id,
+                        start.time=unclass(Sys.time()), stringsAsFactors=F)
+    instance.log <- rbind(instance.log, new.row)    
+  }  
+  
+  # wait for a log to appear from the first machine...
+  print("waiting 90 seconds for machines to boot and download packages...")
+  Sys.sleep(90)
+  print("looking for a log entry from first machine to appear in log...")
+  wait.for.log <- TRUE
+  while(wait.for.log){
+    machine <- instance.log$instance.id[1]
+    read.message.log.json <- read.message.log.from.dynamo(path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
+                                    table.name=log.table.name, instance.id=machine)
+    # amazon may return single quotes instead of required double quotes
+    log.list <- fromJSON(gsub("'", '"', read.message.log.json))
+    log.count <- log.list$Count
+    if(log.count!=0){
+      print("first machine is online!")
+      boot.time <- round(unclass(Sys.time()) - start.time)
+      print(paste0("it took ", boot.time, " seconds to boot the first machine"))
+      wait.for.log <- FALSE
+    }else{
+      print("no log found, waiting another 10 seconds... ")
+      Sys.sleep(10)
+    }
   }
-}
+
   ### monitoring
   # read the time of the most recently completed task for each machine
   # if the time difference from current time has been greater than
   # allowable.time, then kill the machine and if there are still items
   # left in the queue, start another machine
-  while(length(ec2.machine.id.vec) > 0){
-    for(machine in ec2.machine.id.vec){
+  while(nrow(instance.log) > 0){
+    for(machine in instance.log$instance.id){
       read.message.log.json <- read.message.log.from.dynamo(path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
                                     table.name=log.table.name, instance.id=machine)
       # amazon may return single quotes instead of required double quotes
-      log.list <- fromJSON(gsub("'", '"', read.message.log.json$Count))
+      log.list <- fromJSON(gsub("'", '"', read.message.log.json))
       log.count <- log.list$Count
       if(log.count>0){
         last.message.time <- log.list$Items[[1]]$datetime
-        if(unclass(Sys.time()) - last.message.time > allowable.time){
+        over.time <- unclass(Sys.time()) - last.message.time > allowable.time
+        if(over.time){
+          print("a machine has not submitted a log in the allowable time, it's being shut down")
           stop.ec2.machine(instance.id=machine, path.to.ec2.shell.scripts=path.to.ec2.shell.scripts)
           # remove stopped machine from list
-          ec2.machine.id.vec <- ec2.machine.id.vec[ec2.machine.id.vec != machine]
+          instance.log <- instance.log[instance.log$instance.id!=machine,]
           # start new machine if there are still items left in queue
           queue.length <- get.queue.length(path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
                                          aws.account=aws.account, queue=queue)
           if(as.numeric(queue.length) > 10){
-            ec2.machine.id.vec <- c(ec2.machine.id.vec,
-                                  start.ec2.machine(ami.id=ami.id,
-                                  ec2.instance.type=ec2.instance.type,
-                                  aws.availability.zone=aws.availability.zone,
-                                  path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
-                                  key=ec2.key))
+            print("a replacement machine is being booted")
+            new.instance.id <- start.ec2.machine(ami.id=ami.id,
+                                 ec2.instance.type=ec2.instance.type,
+                                 aws.availability.zone=aws.availability.zone,
+                                 path.to.ec2.shell.scripts=path.to.ec2.shell.scripts,
+                                 user.data.file=user.data.file, key=ec2.key,
+                                 group=ec2.security.group)
+            new.row <- data.frame(instance.id=new.instance.id,
+                                  start.time=unclass(Sys.time()), stringsAsFactors=F)
+            instance.log <- rbind(instance.log, new.row)    
           }
         }
       }
@@ -63,7 +97,7 @@ run <- function(queue, max.nodes, ami.id, ec2.instance.type, aws.availability.zo
   }
   # Shouldn't need this as they should all be stopped already...
   # stop machines
-  for(machine in ec2.machine.id.vec){
+  for(machine in instance.log$instance.id){
     stop.ec2.machine(instance.id=machine, path.to.ec2.shell.scripts=path.to.ec2.shell.scripts)
     ec2.machine.id.vec <- NULL
   }
@@ -75,4 +109,4 @@ run(queue=my.queue, max.nodes=my.max.nodes, ami.id=my.ami.id,
     path.to.ec2.shell.scripts=my.path.to.ec2.shell.scripts,
     allowable.time=my.allowable.time, aws.account=my.aws.account,
     user.data.file=my.user.data.file, ec2.key=my.ec2.key,
-    ec2.security.group=my.ec2.security.group)
+    ec2.security.group=my.ec2.security.group, log.table.name=my.log.table.name)
